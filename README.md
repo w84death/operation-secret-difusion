5e188ef33f0c07ee14afb797c2261e9dca36bdc1
Krzysztof Jankowski <kj@p1x.in>
2022-10-23 23:31:33 +0200

    screens + p1x terminator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# operation-secret-difusion
-Game made in LOVE 2D, LUA. All assets made by AI Stable Diffusion.
+# Operation: Secret Difusion
+Game made in LOVE 2D, LUA.
+
+All assets made by AI Stable Diffusion.
+
+![Screenshot from game](screen1.jpg)
+![Screenshot from game](screen2.jpg)
--- game/code/intro.lua
+++ game/code/intro.lua
@@ -1,14 +1,17 @@
 intro = {}
 local w,h = love.graphics.getWidth(), love.graphics.getHeight()
+local hw = w/2
 
 intro.load = function()
   print("LOADING: Intro...")
-  cover_img = love.graphics.newImage("cover.jpg")
+  cover_img = love.graphics.newImage("panels/cover.jpg")
+  terminator_img = love.graphics.newImage("sprites/p1x_terminator.png")
   print("OK\n")
 end
 
 intro.draw = function()
   love.graphics.draw(cover_img, 0, 0)
+  love.graphics.draw(terminator_img, hw-128, h-386)
   ui.draw_logo()
   ui.draw_start()
 end
